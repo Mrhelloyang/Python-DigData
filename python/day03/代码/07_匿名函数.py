049701248418a36fb6d88a6bd2015912ac76fd9f
Mrhelloyang <2131424889@qq.com>
2024-06-23 20:35:00 +0800

    hive练习

--- python/day03/代码/07_匿名函数.py
+++ python/day03/代码/07_匿名函数.py
@@ -1,5 +1,8 @@
 
 #匿名函数
-foo=lambda a,b:a+b
-ret=foo(10,20)
-print(ret)
+# foo=lambda a,b:a+b
+# ret=foo(10,20)
+# print(ret)
+
+student_list=[{'id':'1','name':'张四','age':'21','gender':'男'},{'id':'2','name':'sisi','age':'22','gender':'女'}]
+ret = student_list.index(1)
